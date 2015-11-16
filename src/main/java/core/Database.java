af792dcd0b007448df1934827fc54da26b0c5a9f
Scott <swiwanicki@gmail.com>
2015-11-15 21:16:59 -0500

    Created basic Database Add/Remove Funcitons

--- src/main/java/core/Database.java
+++ src/main/java/core/Database.java
@@ -9,8 +9,8 @@ import java.io.*;
  * Created by Scott on 11/14/2015.
  */
 public class Database {
+    Connection con = null;
     public Database() throws Exception{
-        Connection con = null;
         File tempfile = new File("src/main/java/DatabaseTest/Database");
         String path = tempfile.getAbsolutePath();
             Class.forName("org.apache.derby.jdbc.ClientDriver");
@@ -33,13 +33,49 @@ public class Database {
      * @param locToAdd The location/node that needs to be added to the database
      */
     public void AddNode(Location locToAdd) {
+        //// TODO: 11/15/2015 Add in names functionality
+        double x = locToAdd.getPosition().getX();
+        double y = locToAdd.getPosition().getY();
+        int floorNum = locToAdd.getFloorNumber();
+        String query = "INSERT INTO NODES (POS_X, POS_Y, FLOOR_NUM)" +
+                "(" + x + "," + y + "," + floorNum + ")";
+        String addNames = null;
+        try {
+            Statement stmt = con.createStatement();
+            stmt.execute(query);
+            /* Add location NAMES
+            for (int i = 0; i < locToAdd.) {
 
+            */
+        } catch (SQLException e) {
+            System.out.print("Couldn't add location\n" + e);
+        }
     }
 
     /*
      * @param locToRem The location/node that needs to be removed to the database
      */
     public void RemoveNode(Location locToRem) {
+        double x = locToRem.getPosition().getX();
+        double y = locToRem.getPosition().getY();
+        try {
+            Statement stmt = con.createStatement();
+            String getID = "SELECT NODE_ID FROM NODES WHERE POS_X = "  +
+            x + "and POS_Y =" + y;
+            ResultSet rs = stmt.executeQuery(getID);
+            int nodeId = rs.getInt("NODE_ID");
+            String remNodeEdge = "DELETE FROM NODE_EDGES WHERE NODE_ID ="
+                    + nodeId;
+            String remEdge = "DELETE FROM EDGES WHERE NODE1_ID = "
+                    + nodeId + "or NODE2_ID = " + nodeId;
+            String remNode = "DELETE FROM NODES WHERE POS_X = " +
+                     x + "and POS_Y =" + y;
+            stmt.execute(remNodeEdge);
+            stmt.execute(remEdge);
+            stmt.execute(remNode);
+        } catch (SQLException e) {
+            System.out.print("Couldn't add location\n" + e);
+        }
 
     }
 
@@ -47,13 +83,58 @@ public class Database {
      * @param locToUpdate The location/node that needs to be updated in the database
      */
     public void UpdateNode(Location locToUpdate) {
-
+        //// TODO: 11/15/2015 will add when adding names
+        /*
+        double x = locToUpdate.getPosition().getX();
+        double y = locToUpdate.getPosition().getY();
+        try {
+            Statement stmt = con.createStatement();
+            String getID = "SELECT NODE_ID FROM NODES WHERE POS_X = "  +
+                    x + "and POS_Y =" + y;
+            ResultSet rs = stmt.executeQuery(getID);
+            int nodeId = rs.getInt("NODE_ID");
+            String remNodeEdge = "DELETE FROM NODE_EDGES WHERE NODE_ID ="
+                    + nodeId;
+            String remEdge = "DELETE FROM EDGES WHERE NODE1_ID = "
+                    + nodeId + "or NODE2_ID = " + nodeId;
+            String remNode = "DELETE FROM NODES WHERE POS_X = " +
+                    x + "and POS_Y =" + y;
+            stmt.execute(remNodeEdge);
+            stmt.execute(remEdge);
+            stmt.execute(remNode);
+        } catch (SQLException e) {
+            System.out.print("Couldn't add location\n" + e);
+        }
+        */
     }
 
     /*
      * @param edgeToAdd The edge that needs to be added to the database
      */
     public void AddEdge(Edge edgeToAdd) {
+        //// TODO: 11/15/2015 add in attributes functionality
+        double x1 = edgeToAdd.getNode1().getPosition().getX();
+        double y1 = edgeToAdd.getNode1().getPosition().getY();
+        double x2 = edgeToAdd.getNode2().getPosition().getX();
+        double y2 = edgeToAdd.getNode2().getPosition().getY();
+        try {
+            Statement stmt = con.createStatement();
+            String getID1 = "SELECT NODE_ID FROM NODES WHERE POS_X = "  +
+                    x1 + "and POS_Y =" + y1;
+            String getID2 = "SELECT NODE_ID FROM NODES WHERE POS_X = "  +
+                    x2 + "and POS_Y =" + y2;
+            ResultSet rs = stmt.executeQuery(getID1);
+            int nodeId1 = rs.getInt("NODE_ID");
+            rs = stmt.executeQuery(getID2);
+            int nodeId2 = rs.getInt("NODE_ID");
+
+            String query = "INSERT INTO NODES (NODE1_ID, NODE2_ID)" +
+                    "(" + nodeId1 + "," + nodeId2 + ")";
+            stmt.execute(query);
+        } catch (SQLException e) {
+            System.out.print("Couldn't add location\n" + e);
+        }
+
 
     }
 
@@ -61,6 +142,32 @@ public class Database {
      * @param edgeToRem The edge that needs to be removed to the database
      */
     public void RemoveEdge(Edge edgeToRem) {
+        double x1 = edgeToRem.getNode1().getPosition().getX();
+        double y1 = edgeToRem.getNode1().getPosition().getY();
+        double x2 = edgeToRem.getNode2().getPosition().getX();
+        double y2 = edgeToRem.getNode2().getPosition().getY();
+        try {
+            Statement stmt = con.createStatement();
+            String getID1 = "SELECT NODE_ID FROM NODES WHERE POS_X = "  +
+                    x1 + "and POS_Y =" + y1;
+            String getID2 = "SELECT NODE_ID FROM NODES WHERE POS_X = "  +
+                    x2 + "and POS_Y =" + y2;
+            ResultSet rs = stmt.executeQuery(getID1);
+            int nodeId1 = rs.getInt("NODE_ID");
+            rs = stmt.executeQuery(getID2);
+            int nodeId2 = rs.getInt("NODE_ID");
+            
+            /*
+            String remNodeEdge = "DELETE FROM NODE_EDGES WHERE EDGE_ID ="
+                    + nodeId;
+                    */
+            String remEdge = "DELETE FROM EDGES WHERE (NODE1_ID = "
+                    + nodeId1 + "AND NODE2_ID = " + nodeId2 + 
+                    ") OR ( NODE1_ID = " + nodeId2 + "AND NODE2_ID = " + nodeId1 + ")";
+            stmt.execute(remEdge);
+        } catch (SQLException e) {
+            System.out.print("Couldn't add location\n" + e);
+        }
 
     }
 
@@ -68,6 +175,7 @@ public class Database {
      * @param edgeToUpdate The edge that needs to be updated in the database
      */
     public void UpdateEdge(Edge edgeToUpdate) {
+        //// TODO: 11/15/2015 Add in update funcitonality 
 
     }
 }

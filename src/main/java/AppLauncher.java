3cea494783a62b1f3fbd76c1e5f202d57170ab5d
emtomko <emtomko@wpi.edu>
2015-12-14 19:39:07 -0500

    improved time limit for load bar, set more accurate percentages

--- src/main/java/AppLauncher.java
+++ src/main/java/AppLauncher.java
@@ -17,13 +17,14 @@ public class AppLauncher{
             JSplash splashscreen = new JSplash(AppLauncher.class.getResource ("BrokeEverything.png"),
                     true, true, false, "AZTEC WASH Mapper", null, Color.BLACK, Color.BLUE);
             splashscreen.splashOn();
-            splashscreen.setProgress(20, "Initializing....");
-            Thread.sleep(1000);
+            splashscreen.setProgress(15, "Initializing....");
+            Thread.sleep(10);
             Database graphData = new Database();
             //Make a frame
             MainAppUI app = new MainAppUI(graphData.createGraph());
             graphData.closeConnection();
             splashscreen.setProgress(40, "Loading....");
+            Thread.sleep(10);
 
             //change the look and feel to the Nimbus style
             try {
@@ -34,14 +35,14 @@ public class AppLauncher{
                 e.printStackTrace();
             }
 
-            splashscreen.setProgress(60, "Applying Configurations....");
-            Thread.sleep(1000);
+            splashscreen.setProgress(50, "Applying Configurations....");
+            Thread.sleep(10);
 
             app.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
             app.setMinimumSize(new Dimension(1024, 768));
             app.setExtendedState(JFrame.MAXIMIZED_BOTH);
-            splashscreen.setProgress(80, "Starting Application....");
-            Thread.sleep(1000);
+            splashscreen.setProgress(90, "Starting Application....");
+            Thread.sleep(10);
 
             app.setUpMainApp();
             app.repaint();
